733deaf177c59ff757bbb4f9fd1f41d7e42af22f
Saidul Islam Santo <sksanto.pro@gmail.com>
2021-08-31 02:13:02 +0600

    Js script and nav animation

---
+++ script.js
@@ -0,0 +1,16 @@
+
+/*---------------Navbar Hide and Unhide-----------*/
+
+/* Note: When the user scrolls down, hide the navbar.
+ When the user scrolls up, show the navbar */
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("main-head").style.top = "0";
+  } else {
+    document.getElementById("main-head").style.top = "-98px";
+  }
+  prevScrollpos = currentScrollPos;
+}
