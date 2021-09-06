34b37311d5c2438512dd96e1ab72566851c3008e
Saidul Islam Santo <sksanto.pro@gmail.com>
2021-09-07 02:46:08 +0600

    js change

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("main-head").style.top = "0";
   } else {
-    document.getElementById("main-head").style.top = "-100px";
+    document.getElementById("main-head").style.top = "-120px";
   }
   prevScrollpos = currentScrollPos;
 }
